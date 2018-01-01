8c6d550a0030ec8f7c7b41b73529826104987756
littlestar-642 <avinashjasiwal642@gmail.com>
2018-01-01 22:06:04 +0530

    change port attributes

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ io.on('connection',(socket)=>{
 });
 
 
-
-http.listen(3000,()=>console.log('listening on *:3000'));
+var port=process.env.PORT||3000;
+http.listen(3000,()=>console.log(`listening on ${port}` ));
